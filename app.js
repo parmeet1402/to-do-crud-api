3970cc7d7055e223ef7ca2844926086671ed274f
Parmeet <parmeet.asi@gmail.com>
2019-03-12 15:53:17 +0530

    find and find one working

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log("asdf");
+// import express
+const express = require("express");
+// import mongoDbClient
+const MongoClient = require("mongodb").MongoClient;
+
+// import database connection string
+const uri = require("./config/keys").mongoURI;
+
+// database connection object
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+// creating connection
+client.connect(err => {
+  const database = client.db("test");
+  const collection = database.collection("devices");
+
+  // running findOne command
+  collection.findOne({}, (err, result) => {
+    console.log("running find one command");
+    console.log(result);
+  });
+
+  // runinng find command
+  collection.find({}).toArray((err, result) => {
+    console.log("running find command");
+    if (err) throw err;
+    console.log(result);
+  });
+  client.close();
+});
